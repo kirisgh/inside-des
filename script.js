cf57040a96398debac664804110cc608ab1b570a
kirisgh <klflaviano@gmail.com>
2025-03-28 09:53:00 +0800

    uy nag scroll

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const toggleMenu = document.querySelector(".toggle_menu");
 const headerNav = document.querySelector(".header_nav");
 const backDrop = document.querySelector(".backDrop");
+const headerWrapper = document.querySelector(".header_wrapper")
 
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("open")
     headerNav.classList.toggle("open")
     backDrop.classList.toggle("open")
 });
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 75) {
+        headerWrapper.classList.add("open");
+    } else {
+        headerWrapper.classList.remove("open");
+    }
+});
+
